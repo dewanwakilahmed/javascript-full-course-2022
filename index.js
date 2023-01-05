d336eaef7d95183d24556beddfc1ed01010ae97d
dewanwakilahmed <dewanwakilahmed@gmail.com>
2023-01-06 01:39:37 +0600

    Quick introduction

---
+++ index.js
@@ -0,0 +1,16 @@
+// QUICK INTRODUCTION (starts here)
+console.log('I like pizza!');
+console.log("It's really good!");
+
+window.alert('I really love pizza!');
+
+// This is a comment
+
+/*
+  This
+  is
+  a
+  multiline
+  comment
+*/
+// QUICK INTRODUCTION (ends here)
