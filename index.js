be25346e57c10bdcb2c23fb4eabbe197613d7633
dewanwakilahmed <dewanwakilahmed@gmail.com>
2023-01-06 02:46:29 +0600

    Arithmetic expressions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// QUICK INTRODUCTION (starts here)
+/* QUICK INTRODUCTION */
 console.log('I like pizza!');
 console.log("It's really good!");
 
@@ -13,9 +13,8 @@ console.log("It's really good!");
   multiline
   comment
 */
-// QUICK INTRODUCTION (ends here)
 
-// VARIABLES (starts here)
+/* VARIABLES */
 // A variable is a container for storing data
 // A variable behaves as if it was the value that it contains
 
@@ -34,4 +33,41 @@ console.log('Enrolled:', student);
 document.getElementById('p1').innerHTML = 'Hello ' + firstName;
 document.getElementById('p2').innerHTML = 'You are ' + age + ' years old!';
 document.getElementById('p3').innerHTML = 'Enrolled: ' + student;
-// VARIABLES (ends here)
+
+/* ARITHMETIC EXPRESSIONS */
+/*
+  arithmetic expression is a combination of...
+  operands (values, variables, etc.) and
+  operators (+ - * / %)
+  that can be evaluated to a value
+  ex. y = x + 5
+*/
+
+let students = 20;
+
+students = students + 1;
+students = students - 1;
+students = students * 2;
+students = students / 2;
+
+let extraStudents = students % 3;
+
+// Augmented assignment operators
+students += 1;
+students -= 1;
+students *= 2;
+students /= 2;
+
+console.log(students);
+console.log(extraStudents);
+
+/*
+  operator precedence
+  1. parenthesis ()
+  2. exponents
+  3. multiplication & division
+  4. addition & substraction
+*/
+
+let result = (1 + 2) * (3 + 4);
+console.log(result);
