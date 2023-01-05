d961b2aa8c962975ce87ad1f7c73ce95ceceb3c3
dewanwakilahmed <dewanwakilahmed@gmail.com>
2023-01-06 01:53:16 +0600

    Variables

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 console.log('I like pizza!');
 console.log("It's really good!");
 
-window.alert('I really love pizza!');
+// window.alert('I really love pizza!');
 
 // This is a comment
 
@@ -14,3 +14,24 @@ window.alert('I really love pizza!');
   comment
 */
 // QUICK INTRODUCTION (ends here)
+
+// VARIABLES (starts here)
+// A variable is a container for storing data
+// A variable behaves as if it was the value that it contains
+
+// Two steps:
+// 1. Declaration (var, let, const)
+// 2. Assignment (= assignment operator)
+
+let firstName = 'Bro'; // string
+let age = 21; // number
+let student = true; // boolean
+
+console.log('Hello', firstName);
+console.log('You are', age, 'years old');
+console.log('Enrolled:', student);
+
+document.getElementById('p1').innerHTML = 'Hello ' + firstName;
+document.getElementById('p2').innerHTML = 'You are ' + age + ' years old!';
+document.getElementById('p3').innerHTML = 'Enrolled: ' + student;
+// VARIABLES (ends here)
